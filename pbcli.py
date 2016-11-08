53bf777b405cd67c794415c2e8ffc6592a29bc56
Daan Wielens <daanwielens@192.168.1.9>
2016-11-08 21:14:50 +0100

    Push files
    pbcli is now capable of pushing files. Provide the filename, the path
    to the file and a message and pbcli will send the file. This version
    only supports jpg, png and pdf; other filetypes can be added easily.

--- pbcli.py
+++ pbcli.py
@@ -11,16 +11,49 @@ global TOKEN
 with open('pb_token.txt', 'r') as file:
     TOKEN = file.read().replace('\n','')
 
-# This script is a function to send notes easily from any application/script:
-# - Place this file in the project folder
-# - import pbcli
-# - pbcli.note("title","message")
+# Function to send a note. Usage: import pbcli --> pbcli.note(ttl,msg)
 def note(ttl,msg):
     url = "https://api.pushbullet.com/v2/pushes"
     data = dict(type="note", title=ttl, body=msg)
     nreq = requests.post(url, json=data, auth=(TOKEN, '')).json()
 
-if len(sys.argv)!=3:
-    print('Usage: pbcli.py "Title" "Message"')
+# Function to send a file
+def pfile(fname,fpath,msg):
+    # First request authorization to upload a file
+    url = "https://api.pushbullet.com/v2/upload-request"
+    ext = fpath.split('.')[-1]
+    if ext == 'jpg':
+        f_type = 'image/jpeg'
+    elif ext == 'png':
+        f_type = 'image/png'
+    elif ext == 'pdf':
+        f_type = 'application/pdf'
+    else:
+        print('Supported file types:\njpg, png, pdf')
+        sys.exit()
+
+    f_name = fpath.split('/')[-1]
+    data = dict(file_name=f_name, file_type=f_type)
+    nreq = requests.post(url, json=data, auth=(TOKEN, '')).json()
+    rf_name = nreq['file_name']
+    rf_type = nreq['file_type']
+    rf_durl = nreq['file_url']
+    rf_uurl = nreq['upload_url']
+
+    # Upload the file_url
+    f = {'file': open(fpath,'rb')}
+    ureq = requests.post(rf_uurl, files=f)
+
+    # Push the file
+    url = "https://api.pushbullet.com/v2/pushes"
+    data = dict(type="file", body=msg, file_name=fname, file_type = rf_type, file_url = rf_durl)
+    freq = requests.post(url, json=data, auth=(TOKEN, '')).json()
+
+if len(sys.argv) == 1:
+    print('Usage:\n - Send a note: python pbcli.py -n title message\n - Send a file: python pbcli.py -f filename /path/to/file message')
+elif (sys.argv[1] == '-n') and (len(sys.argv) == 4):
+    note(sys.argv[2],sys.argv[3])
+elif (sys.argv[1] == '-f') and (len(sys.argv) == 5):
+    pfile(sys.argv[2], sys.argv[3], sys.argv[4])
 else:
-    note(sys.argv[1],sys.argv[2])
+    print('Usage:\n - Send a note: python pbcli.py -n title message\n - Send a file: python pbcli.py -f filename /path/to/file message')
